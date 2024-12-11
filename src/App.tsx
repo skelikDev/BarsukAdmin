4a825f2796d0e12ccc6d4a51272ddebb32b0e1b4
Aleksandr Kim <aleksandr.kim@indriver.com>
2024-12-11 12:36:51 +0500

    Reinit with vite shad tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar.tsx';
 import { AppSidebar } from '@/components/ui-composition/app-sidebar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { UserSearchForm } from '@/components/user-search-form';
+import { createReactRouter, createRouteConfig } from '@tanstack/react-router';
+
+const routeConfig = createRouteConfig().addChildren([
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+]);
 
 function App() {
 
---
+++ src/api/generated/queries/prefetch.ts
@@ -0,0 +1,10 @@
+// generated with @7nohe/openapi-react-query-codegen@2.0.0-beta.3 
+
+import { type Options } from "@hey-api/client-fetch";
+import { type QueryClient } from "@tanstack/react-query";
+import { productControllerGetProducts, usersControllerGetAllUsers, usersControllerGetUserById } from "../requests/services.gen";
+import { ProductControllerGetProductsData, UsersControllerGetAllUsersData, UsersControllerGetUserByIdData } from "../requests/types.gen";
+import * as Common from "./common";
+export const prefetchUseUsersControllerGetAllUsers = (queryClient: QueryClient, clientOptions: Options<UsersControllerGetAllUsersData, true> = {}) => queryClient.prefetchQuery({ queryKey: Common.UseUsersControllerGetAllUsersKeyFn(clientOptions), queryFn: () => usersControllerGetAllUsers({ ...clientOptions }).then(response => response.data) });
+export const prefetchUseUsersControllerGetUserById = (queryClient: QueryClient, clientOptions: Options<UsersControllerGetUserByIdData, true>) => queryClient.prefetchQuery({ queryKey: Common.UseUsersControllerGetUserByIdKeyFn(clientOptions), queryFn: () => usersControllerGetUserById({ ...clientOptions }).then(response => response.data) });
+export const prefetchUseProductControllerGetProducts = (queryClient: QueryClient, clientOptions: Options<ProductControllerGetProductsData, true> = {}) => queryClient.prefetchQuery({ queryKey: Common.UseProductControllerGetProductsKeyFn(clientOptions), queryFn: () => productControllerGetProducts({ ...clientOptions }).then(response => response.data) });
